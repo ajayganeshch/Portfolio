af14b00d42261f16597836398f5518f663f5a458
Ajay Ganesh <ajayganesh789@gmail.com>
2023-10-13 22:14:06 +0530

    Update Bootstrap Path

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import "./bootstrap/dist/css/bootstrap.min.css";
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./NavBar.css";
 import { Link } from "react-scroll";
 
