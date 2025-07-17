7a3dcbbfe3e8fca4728b319815d540fa189a4e58
Ajay Ganesh <ajayganesh789@gmail.com>
2025-07-18 00:02:32 +0530

    Refactor NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,52 +15,63 @@ export function NavBar() {
   };
 
   return (
-    <Navbar expand="md" className="nav-div">
-      <Navbar.Brand className="logo" onClick={scrollToTop}>
-        AG
-      </Navbar.Brand>
-      <Navbar.Toggle
-        className="me-4"
-        onClick={() => setIsOpen(!isOpen)}
-        aria-controls="basic-navbar-nav"
+    <>
+      <Navbar expand="md" className="nav-div">
+        <Navbar.Brand className="logo" onClick={scrollToTop}>
+          AG
+        </Navbar.Brand>
+        <Navbar.Toggle
+          className="me-4"
+          onClick={() => setIsOpen(!isOpen)}
+          aria-controls="basic-navbar-nav"
+        >
+          {isOpen ? (
+            <span className="toggle-icon">&times;</span>
+          ) : (
+            <span className="toggle-icon">☰</span>
+          )}
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto ms-auto links-nav">
+            <Nav.Link
+              className="me-3 link-a"
+              as={Link}
+              to="about-div"
+              smooth={true}
+              duration={100}
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              className="me-3 link-a"
+              as={Link}
+              to="resume-div"
+              smooth={true}
+              duration={100}
+            >
+              Resume
+            </Nav.Link>
+            <Nav.Link
+              className="me-3 link-a"
+              as={Link}
+              to="div-form"
+              smooth={true}
+              duration={100}
+            >
+              Contact Me
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <span
+        style={{
+          fontSize: "small",
+          display: "block",
+          textAlign: "center",
+        }}
       >
-        {isOpen ? (
-          <span className="toggle-icon">&times;</span>
-        ) : (
-          <span className="toggle-icon">☰</span>
-        )}
-      </Navbar.Toggle>
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto ms-auto links-nav">
-          <Nav.Link
-            className="me-3 link-a"
-            as={Link}
-            to="about-div"
-            smooth={true}
-            duration={100}
-          >
-            About
-          </Nav.Link>
-          <Nav.Link
-            className="me-3 link-a"
-            as={Link}
-            to="resume-div"
-            smooth={true}
-            duration={100}
-          >
-            Resume
-          </Nav.Link>
-          <Nav.Link
-            className="me-3 link-a"
-            as={Link}
-            to="div-form"
-            smooth={true}
-            duration={100}
-          >
-            Contact Me
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+        Last Edited - October 14 2023
+      </span>
+    </>
   );
 }
